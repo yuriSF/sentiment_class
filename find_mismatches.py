f588351e8cbd1bd51a2b6839167c9ca57c99d0e9
yuriSF <yuri.yerastov@gmail.com>
2017-05-25 11:47:30 -0500

    added mismatches

--- find_mismatches.py
+++ find_mismatches.py
@@ -1,6 +1,17 @@
 from file_ops import open_csv, write_csv_row
 
-data = open_csv('results/human_judgments3.csv')
+# data = open_csv('results/human_judgments3.csv')
+# for row in data:
+#     if row[7] != row[5] or row[6] != row[4]:
+#         write_csv_row('mismatches.csv', row)
+
+
+data = open_csv('results/classified_coarse.csv')
 for row in data:
-    if row[7] != row[5] or row[6] != row[4]:
-        write_csv_row('mismatches.csv', row)
+    if row[6] != row[4]:
+        write_csv_row('results/mismatches_testset_coarse.csv', row)
+
+data = open_csv('results/classified_fine2.csv')
+for row in data:
+    if row[6] != row[4]:
+        write_csv_row('results/mismatches_testset_fine.csv', row)
